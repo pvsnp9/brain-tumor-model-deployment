ec5c9f74c3f4e904433484dc2bad8b813175c45c
Tsuyog Basnet <tsuyogbasnet@Tsuyogs-MacBook-Pro.local>
2020-08-24 09:24:44 -0600

    v and static files

--- mri_segmentation/settings.py
+++ mri_segmentation/settings.py
@@ -119,10 +119,17 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
-STATIC_URL = '/static/'
+# STATIC_URL = '/static/'
 
-MEDIA_URL = '/images/'
+# MEDIA_URL = '/images/'
 
-STATICFILES_DIRS = [ 
-    os.path.join(BASE_DIR, 'static')
-]
+# STATICFILES_DIRS = [ 
+#     os.path.join(BASE_DIR, 'static')
+# ]
+
+
+# Heroku app
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_URL = "/static/"
+
+django_heroku.settings(locals())
